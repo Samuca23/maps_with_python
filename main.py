3cb36846de3de85095cc66171ce602e447bad5ef
samuca23 <samuelchiodini@hotmail.com>
2024-06-21 00:57:46 -0300

    ajustes

--- main.py
+++ main.py
@@ -24,8 +24,28 @@ for bairro, coords in bairros.items():
     for lat, lon in coords:
         folium.Marker(
             location=[lat, lon],
-            popup=f'<strong>Bairro:</strong> {bairro}<br><strong>Total:</strong> {len(coords)}',
+            popup=f'<strong>Código:</strong>45',
             icon=folium.Icon(color=icon_color)  
         ).add_to(mapa)
 
+# Adiciona uma legenda personalizada
+legend_html = '''
+     <div style="
+     position: fixed; 
+     bottom: 50px; left: 50px; width: 200px; height: 90px; 
+     background-color: white; z-index:9999; font-size:14px;
+     border:2px solid grey; padding: 10px;">
+     <b>Legenda:</b><br>
+     <i class="fa fa-map-marker fa-2x" style="color:#cc3b28"></i>&nbsp; Centro<br>
+     <i class="fa fa-map-marker fa-2x" style="color:#36a5d7"></i>&nbsp; Jardim América
+     </div>
+     '''
+mapa.get_root().html.add_child(folium.Element(legend_html))
+
+title_html = '''
+             <h3 align="center" style="font-size:20px"><b>Mapa de Bairros</b></h3>
+             '''
+mapa.get_root().html.add_child(folium.Element(title_html))
+
+# Salva o mapa
 mapa.save('mapa_com_pontos.html')
